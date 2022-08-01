e5bd8852ab011ee247726139de80080b97901f4b
serapple <serinamgil@gmail.com>
2022-08-01 22:57:00 +0900

    루트 변경, 차후 추가 기능 위한 receive 생성

--- app.py
+++ app.py
@@ -14,15 +14,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/recipeWrite')
+@app.route('/recipe')
 def detail():
-    return render_template('recipeWrite.html')
+    return render_template('recipe.html')
 
 @app.route("/recipe", methods =["post"])
 def recipe_post():
     title_receive = request.form['title_give']
     content_receive = request.form['content_give']
     img_receive = request.form['img_give']
+    # id_receive = request.form['id_give'] -> 유저 아이디
+    # pw_receive = request.form['pw_give'] -> 유저 패스워드
+    # num_receive = request.form['num_give'] -> 유저 작성글 넘버
     doc = {'title': title_receive, 'content': content_receive, 'img': img_receive}
     db.recipe.insert_one(doc)
     return jsonify({'msg': '작성 완료'})
