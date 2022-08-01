135457520260828022d1980a713a8f661016f805
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-08-01 11:50:18 +0900

    기본 코드

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@cluster0.libb3xt.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
+db = client.dbsparta
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
