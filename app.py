c5046706b499767b85582aba5a65db272c5cc835
minseolki <110362442+minseolki@users.noreply.github.com>
2022-08-01 22:20:55 +0900

    Add files via upload
    회원가입 아직 password 암호화 안됨

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
-import certifi
+app = Flask(__name__)
 
+
+import certifi
+from pymongo import MongoClient
 ca = certifi.where()
-client = MongoClient('mongodb+srv://test:sparta@cluster0.libb3xt.mongodb.net/Cluster0?retryWrites=true&w=majority',
-                     tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.libb3xt.mongodb.net/Cluster0?retryWrites=true&w=majority',tlsCAFile=ca)
 db = client.dbsparta
 
-app = Flask(__name__)
+
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('sign_in.html')
+
+@app.route('/sign_in', methods=["POST"])
+def sign_in():
+
+    name_receive = request.form['name_give']
+    id_receive = request.form['id_give']
+    password_receive = request.form['password_give']
+
+
+    doc = {
+        'name' : name_receive,
+        'id': id_receive,
+        'password': password_receive
+    }
+    db.user.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료!'})
 
 
 if __name__ == '__main__':
