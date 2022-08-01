d81b4454e19e4814f20479ff3e61e159cc0972f2
HongJoonHyung <sesa0802@naver.com>
2022-08-01 13:27:00 +0900

    로그인 초기

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
