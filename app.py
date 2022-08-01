fa4bdbaca4a5d9da02b441a145e557eb607cd13a
serapple <serinamgil@gmail.com>
2022-08-01 20:11:15 +0900

    레시피 작성 창으로 이동루트, 레시피 DB 저장 서버 작성

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
-client = MongoClient('mongodb+srv://test:sparta@cluster0.libb3xt.mongodb.net/Cluster0?retryWrites=true&w=majority',
+client = MongoClient('mongodb+srv://sparta:sparta@cluster0.eualsuf.mongodb.net/Cluster0?retryWrites=true&w=majority',
                      tlsCAFile=ca)
 db = client.dbsparta
 
@@ -14,6 +14,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/recipeWrite')
+def detail():
+    return render_template('recipeWrite.html')
+
+@app.route("/recipe", methods =["post"])
+def recipe_post():
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+    img_receive = request.form['img_give']
+    doc = {'title': title_receive, 'content': content_receive, 'img': img_receive}
+    db.recipe.insert_one(doc)
+    return jsonify({'msg': '작성 완료'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
