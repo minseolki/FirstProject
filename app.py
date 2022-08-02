c8ed9694832ed4ea8e4f3b4d09d12a0947e0caa8
serapple <serinamgil@gmail.com>
2022-08-02 12:20:32 +0900

    이미지 미리보기 위한 temp_img로 save/load 진행

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def home():
 def detail():
     return render_template('recipe.html')
 
-@app.route("/recipe", methods =["post"])
-def recipe_post():
+@app.route("/recipe", methods =["POST"])
+def save_recipe():
     title_receive = request.form['title_give']
     content_receive = request.form['content_give']
     img_receive = request.form['img_give']
@@ -29,6 +29,17 @@ def recipe_post():
     doc = {'title': title_receive, 'content': content_receive, 'img': img_receive}
     db.recipe.insert_one(doc)
     return jsonify({'msg': '작성 완료'})
+
+@app.route("/temp_img", methods = ["POST"])
+def save_image():
+    img_receive = request.form['img_give']
+    db.temp_img.update_one({'num' :1}, {'$set': {'img': img_receive}})
+    return jsonify({'msg': '이미지로드 완료'})
+
+@app.route("/temp_img", methods = ["GET"])
+def load_image():
+    image = list(db.temp_img.find({}, {'_id': False}))
+    return jsonify({'temp_img': image})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
